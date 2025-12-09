cc5c0b7459f932b1a5a217b39eb8d99373cd5c88
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-12-09 17:00:10 +0100

    add logging

--- R/00-Namespace.R
+++ R/00-Namespace.R
@@ -4,6 +4,7 @@ utils::globalVariables(
   c()
 )
 
+#' @importFrom futile.logger DEBUG flog.debug flog.info flog.threshold flog.warn INFO
 #' @importFrom httr2 request req_perform req_timeout req_user_agent resp_status resp_body_html
 #' @importFrom jsonlite fromJSON toJSON write_json
 #' @importFrom rvest html_element html_elements html_text2
---
+++ R/00-logging.R
@@ -0,0 +1,21 @@
+logging <- function(msg, ...) {
+  futile.logger::flog.info(msg, ...)
+}
+
+logDebug <- function(msg, ...) {
+  futile.logger::flog.debug(msg, ...)
+}
+
+logWarn <- function(msg, ...) {
+  futile.logger::flog.warn(msg, ...)
+}
+
+init_logging <- function() {
+  if (as.logical(Sys.getenv("SHOW_DEBUG", unset = "FALSE"))) {
+    futile.logger::flog.threshold(futile.logger::DEBUG)
+    logging("Logger initialized: threshold = DEBUG")
+  } else {
+    futile.logger::flog.threshold(futile.logger::INFO)
+    logging("Logger initialized: threshold = INFO")
+  }
+}
