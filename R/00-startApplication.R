5bf57c11e781d431b5e81fd3bf5ad8ce858eb977
Antonia Runge <antonia.runge@inwt-statistics.de>
2026-02-11 20:35:31 +0100

    use fixed host

--- R/00-startApplication.R
+++ R/00-startApplication.R
@@ -1,19 +1,17 @@
 #' Start Application
 #'
 #' @param port port
-#' @param host host
 #' @param launch.browser If true, the system's default web browser will be launched
 #'
 #' @export
 startApplication <- function(
   port = getOption("shiny.port"),
-  host = "0.0.0.0",
   launch.browser = getOption("shiny.launch.browser", interactive())
 ) {
   shiny::runApp(
     system.file("app", package = "PEITHO"),
     port = port,
-    host = host,
+    host = "0.0.0.0",
     launch.browser = launch.browser
   )
 }
