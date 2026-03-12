8ffd0b5de2845b78556d27ce1d3ae92ac9e05a00
Antonia Runge <antonia.runge@inwt-statistics.de>
2026-03-12 07:55:20 +0100

    select field with DT

--- R/02-results_table-module.R
+++ R/02-results_table-module.R
@@ -17,7 +17,10 @@ results_table_ui <- function(id, title = "") {
       step = 10
     ),
     DT::DTOutput(ns("results_table")),
-    uiOutput(ns("select"))
+    tags$hr(),
+    tags$h5("Selected Cell Content"),
+    verbatimTextOutput(ns("selected_cell_value"), placeholder = TRUE)#,
+    #uiOutput(ns("select"))
   )
 }
 #' Results Table Server Module
@@ -38,6 +41,7 @@ results_table_server <- function(id, wf_run, is_active_tab) {
       DT::datatable(
         df,
         rownames = TRUE,
+        selection = list(mode = "single", target = "cell"),
         options = list(
           paging = FALSE,
           scrollY = "360px",
@@ -50,92 +54,106 @@ results_table_server <- function(id, wf_run, is_active_tab) {
         )
     })
 
-    output$select <- renderUI({
-      # hide the edit UI when no workflow is loaded
-      if (is.null(wf_run())) return(NULL)
+    output$selected_cell_value <- renderText({
+      click <- input$results_table_cell_clicked
 
-      field_select_ui(ns("field_select"))
+      if (is.null(wf_run())) {
+        return("Run a workflow to enable cell selection.")
+      }
+
+      if (is.null(click$value)) {
+        return("Click a cell in the table to view its content.")
+      }
+
+      as.character(click$value)
     })
 
-    sel <- field_select_server(
-      "field_select",
-      reactive(as.data.frame(wf_run(), max_char = input$max_char, max_items = 5)),
-      is_active_tab
-    )
+    # output$select <- renderUI({
+    #   # hide the edit UI when no workflow is loaded
+    #   if (is.null(wf_run())) return(NULL)
 
-    observe({
-      req(isTRUE(is_active_tab()), wf_run())
-      PEITHO:::logDebug(
-        "%s: Selected field: %s",
-        id,
-        paste(sel$selected_row(), sel$selected_column(), sep = ", ")
-      )
-    })
+    #   field_select_ui(ns("field_select"))
+    # })
+
+    # sel <- field_select_server(
+    #   "field_select",
+    #   reactive(as.data.frame(wf_run(), max_char = input$max_char, max_items = 5)),
+    #   is_active_tab
+    # )
+
+    # observe({
+    #   req(isTRUE(is_active_tab()), wf_run())
+    #   PEITHO:::logDebug(
+    #     "%s: Selected field: %s",
+    #     id,
+    #     paste(sel$selected_row(), sel$selected_column(), sep = ", ")
+    #   )
+    # })
   })
 }
 
-field_select_ui <- function(id) {
-  ns <- NS(id)
-  tagList(
-    tags$hr(),
-    tags$h4("Select Field"),
-    fluidRow(
-      column(
-        3,
-        selectInput(ns("row_select"), "Select row", choices = NULL, selected = NULL)
-      ),
-      column(
-        3,
-        selectInput(ns("column_select"), "Select column", choices = NULL, selected = NULL)
-      )
-    )
-  )
-}
+# field_select_ui <- function(id) {
+#   ns <- NS(id)
+#   tagList(
+#     tags$hr(),
+#     tags$h4("Select Field"),
+#     fluidRow(
+#       column(
+#         3,
+#         selectInput(ns("row_select"), "Select row", choices = NULL, selected = NULL)
+#       ),
+#       column(
+#         3,
+#         selectInput(ns("column_select"), "Select column", choices = NULL, selected = NULL)
+#       )
+#     )
+#   )
+# }
 
-field_select_server <- function(id, df, is_active_tab) {
-  moduleServer(id, function(input, output, session) {
-    observe({
-      req(isTRUE(is_active_tab()))
+# field_select_server <- function(id, df, is_active_tab) {
+#   moduleServer(id, function(input, output, session) {
+#     observe({
+#       req(isTRUE(is_active_tab()))
 
-      if (is.null(df())) {
-        PEITHO:::logDebug("%s: Reset inputs if empty wf", id)
-        # hide the step select input when no df is loaded
-        updateSelectInput(session, "row_select", choices = NULL, selected = NULL)
-        updateSelectInput(session, "column_select", choices = NULL, selected = NULL)
-      }
-    })
+#       if (is.null(df())) {
+#         PEITHO:::logDebug("%s: Reset inputs if empty wf", id)
+#         # hide the step select input when no df is loaded
+#         updateSelectInput(session, "row_select", choices = NULL, selected = NULL)
+#         updateSelectInput(session, "column_select", choices = NULL, selected = NULL)
+#       }
+#     })
 
-    observe({
-      req(isTRUE(is_active_tab()), df())
-      PEITHO:::logDebug("%s: Update inputs", id)
+#     observe({
+#       req(isTRUE(is_active_tab()), df())
+#       PEITHO:::logDebug("%s: Update inputs", id)
 
-      df_val <- df()
-      row_choices <- rownames(df_val)
+#       df_val <- df()
+#       row_choices <- rownames(df_val)
 
-      if (isTRUE(input$row_select %in% row_choices)) {
-        selected_row <- input$row_select
-      } else {
-        selected_row <- NULL
-      }
-      updateSelectInput(
-        session, "row_select", choices = row_choices, selected = selected_row
-      )
+#       if (isTRUE(input$row_select %in% row_choices)) {
+#         selected_row <- input$row_select
+#       } else {
+#         selected_row <- NULL
+#       }
+#       updateSelectInput(
+#         session, "row_select", choices = row_choices, selected = selected_row
+#       )
 
-      column_choices <- colnames(df_val)
+#       column_choices <- colnames(df_val)
 
-      if (isTRUE(input$column_select %in% column_choices)) {
-        selected_column <- input$column_select
-      } else {
-        selected_column <- NULL
-      }
-      updateSelectInput(
-        session, "column_select", choices = column_choices, selected = selected_column
-      )
-    })
+#       if (isTRUE(input$column_select %in% column_choices)) {
+#         selected_column <- input$column_select
+#       } else {
+#         selected_column <- NULL
+#       }
+#       updateSelectInput(
+#         session, "column_select", choices = column_choices, selected = selected_column
+#       )
+#     })
 
-    list(
-      selected_row = reactive(input$row_select),
-      selected_column = reactive(input$column_select)
-    )
-  })
-}
+#     list(
+#       selected_row = reactive(input$row_select),
+#       selected_column = reactive(input$column_select)
+#     )
+#   })
+# }
